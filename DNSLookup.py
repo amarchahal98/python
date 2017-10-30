b57214ad4ea035aafbbdb8807c23c58005fec382
Amar Chahal <amarchahal98@gmail.com>
2017-10-29 22:33:59 -0400

    added DNSLookup.py and pytest.py files

---
+++ DNSLookup.py
@@ -0,0 +1,18 @@
+### DNS Lookup Tool ###
+
+# (Import Modules)
+import sys
+import socket
+
+
+# (For using arguments as host input)
+host=str(sys.argv[1])
+
+# (DNS Lookup [Name to IP])
+print(socket.gethostbyname(host))
+
+# (Reverse DNS Lookup [IP to Name])
+# result = socket.gethostbyaddr(host)
+
+print(result[0])
+
---
+++ pytest.py
@@ -0,0 +1,9 @@
+import sys
+import socket
+# query = str(sys.argv[1])
+
+host = socket.gethostname()
+
+print(host)
+
+# print(query)
