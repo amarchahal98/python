5fb67147f962c2e68f77faf3cabf079a8392a564
Danny Lieu <dannylieu@outlook.com>
2017-10-30 12:36:35 -0700

    adding isatty.py

---
+++ isatty.py
@@ -0,0 +1,10 @@
+# Testing isatty functionality (Is stdin being piped?)
+
+import sys
+# (Interactive
+if sys.stdin.isatty():
+    print("isatty")
+
+# (Data is being piped)
+elif not sys.stdin.isatty():
+    print("not isatty")
