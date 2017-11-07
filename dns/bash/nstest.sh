6fbece02317065cc4ba8e80cca14851dcf1396c9
Amar Chahal <amarchahal98@gmail.com>
2017-11-06 16:34:32 -0800

    1

--- dns/bash/nstest.sh
+++ dns/bash/nstest.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # vars
-nameserver=$@
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval  'ns=($(cat nameserver.txt))'
+array=( felix.vcn.bc.ca sylvester.vcn.bc.ca fexlix.vcn.bc.ca ns1.vancouvercommunity.net )
+#IFS=$'\r\n' GLOBIGNORE='*' command eval  'ns=($(cat nameserver.txt))'
 
-if [[ $nameserver != ${ns[0]} && $nameserver != ${ns[1]} && $nameserver != ${ns[2]} && $nameserver != ${ns[3]} ]] ; then
-  echo "no match"
 
-  else 
-    echo "match";
+for arg in $@
+do
+  counter=$((counter+1))
+  echo "${array[$counter]}"
+  #dig -t NS +short $arg | grep "${array[@]}"
 
-fi
+done
+
+
