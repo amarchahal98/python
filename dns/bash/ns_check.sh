fcac7a4bfe9586b516d2bf5a9697086aa04c7612
Amar Chahal <amarchahal98@gmail.com>
2017-11-09 14:57:51 -0800

    backupupload

--- dns/bash/ns_check.sh
+++ dns/bash/ns_check.sh
@@ -1,50 +1,14 @@
 #!/bin/bash
 
-# usage: cat "domain" | bash ns_check.sh
+# usage 1: cat "domain" | bash ns_check.sh
+# usage 2: bash ns_check.sh (type one at a time in shell)
 
-#vars
-ns1=felix.vcn.bc.ca
-ns2=sylvester.vcn.bc.ca
-ns3=fexlix.vcn.bc.ca
-ns4=ns1.vancouvercommunity.net
+#VCN's Nameservers
+ns_array=(ns1.vancouvercommunity.net. ns2.vancouvercommunity.net. ns1.vcn.bc.ca. ns2.vcn.bc.ca. ns3.vcn.bc.ca ns4.vcn.bc.ca. )
 
+# Will check to see if argument matches any of VCN's Nameservers.
 while read domain 
 do 
-
-  ns=$(dig -t NS $domain)
-
-  echo $ns  | grep -o $ns1
-  var1=$?
-
-  if [[ $var1 == 0 ]]; then
-    echo "$domain: match=yes"
-    continue
-  fi
-
-  echo $ns | grep -o $ns2
-  var2=$?
-  if [[ $var2 == 0 ]]; then
-    echo "$domain: match=yes"
-    continue
-  fi
-
-  echo $ns | grep -o $ns3
-  var3=$?
-  if [[ $var3 == 0 ]]; then
-    echo "$domain: match=yes"
-    continue
-
-  fi
-
-  echo $ns | grep -o $ns4
-  var4=$?
-  if [[ $var4 == 0 ]]; then
-    echo "$domain: match=yes"
-    continue
-  fi
-
-  echo "$domain: match=no"  
+  echo $domain
 
 done
-
-
