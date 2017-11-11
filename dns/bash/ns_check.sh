5a5c4862b877d9c6bb24b67fc05d53628297fb2b
Amar Chahal <amarchahal98@gmail.com>
2017-11-10 16:26:24 -0800

    .bak

--- dns/bash/ns_check.sh
+++ dns/bash/ns_check.sh
@@ -4,11 +4,23 @@
 # usage 2: bash ns_check.sh (type one at a time in shell)
 
 #VCN's Nameservers
-ns_array=(ns1.vancouvercommunity.net. ns2.vancouvercommunity.net. ns1.vcn.bc.ca. ns2.vcn.bc.ca. ns3.vcn.bc.ca ns4.vcn.bc.ca. )
+ns_array=( "ns2.vancouvercommunity.net." "ns1.vcn.bc.ca." "ns2.vcn.bc.ca." "ns3.vcn.bc.ca" "ns4.vcn.bc.ca." "ns1.vancouvercommunity.net." )
 
-# Will check to see if argument matches any of VCN's Nameservers.
-while read domain 
-do 
-  echo $domain
 
+while read domain
+do
+dig=$(dig -t NS +short $domain)
+  for line in $dig;
+    do
+	  if [[ $line == ${ns_array[@]} ]]; then
+        echo "match: $line"
+      else
+        echo "no match: $line"
+
+      fi
+
+   done
 done
+
+#echo ${ns_array[@]}
+
