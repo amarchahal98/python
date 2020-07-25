dbdf2e6282153666a4a6a81c276e0aa995011df1
Amar Chahal <achahal@localhost.localdomain>
2020-07-24 21:19:36 -0700

    add

---
+++ learnPythonTheHardWay/exercise01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# A Good First Program - This script prints various strings.
+
+print("Hello")
+print("Hello again")
+
+print('Single quotes this time.')
+print('Single "quotes" don\'t allow special character interpretation')
+
