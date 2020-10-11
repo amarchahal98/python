e3a76f363134bb0369b699536ae974847cb4c598
root <root@localhost.localdomain>
2020-10-11 16:55:31 -0700

    restruct

--- learnPythonTheHardWay/exercise01.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# A Good First Program - This script prints various strings.
-
-print("Hello")
-print("Hello again")
-
-print('Single quotes this time.')
-print('Single "quotes" don\'t allow special character interpretation')
-
--- learnPythonTheHardWay/exercise9.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# Exercise 9 - NewLine Printing - Demonstrating newline
-
-days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-
-print("Days: ", days)
-print("Months: ", months)
